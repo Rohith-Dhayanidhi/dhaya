b56ececc48a1c0473d8403a315f4f329297a1542
DonDhaya1320 <rohithdhayanidhi@gmail.com>
2024-07-09 15:11:15 +0530

    bootstrap

---
+++ app.js
@@ -0,0 +1,202 @@
+// let bike={
+//     name:"Rolce Royce",
+//     Price:100000,
+//     color:"aqua"
+// }
+// let num=[1,2,3,4,5]
+
+// num[2]=20
+
+// bike.Price= 200000
+
+// console.log("The bike name is "+ bike.name , "price " + bike.Price);
+
+// console.log(num[2]);
+
+// console.log(typeof(bike));
+//  let a=10;
+//  let b=20;
+//  console.log(a+b , a-b , a*b , b/a , b%a);
+
+//  function call(a,b) {
+//     var c=a+b
+//     var c1=a-b
+
+//     return [c,c1]
+//  }
+//   console.log(call(20,30));
+//   console.log(call(100,20));
+
+// let num3 = [1,2,3,4,5,6,7,8];
+
+// num5=num3.filter(function number(nu)
+// {
+//     if(nu<6)
+//     {
+//         return nu
+//     }
+// })
+
+// let str="'My','JavaScript'"
+// let num=[1,2,3,4,5,6,7,8,9,10]
+
+// let num2=num.map(
+//     function change(f)
+// {
+// //     let str2=replace.str(
+// //     function str2(rep){
+// //         return str.replace(JavaScript,World)
+// //     }
+// // )
+//     return f*2
+// }
+// )
+// console.log(num2);
+
+// let str="There is a way to learn JavaScript by learning JavaScript"
+// let num=[1,2,3,4,5,6,7,8,9,10]
+
+// function change(string,array){
+//     let changestr=str.replaceAll("JavaScript",string)
+
+//     let multi=num.map(
+//         function mul(number){
+//         return number*array
+//         }
+//     )
+//     console.log(changestr,multi);
+// }
+// change("FSD",5)
+
+// let a=['apple','jackfruit','banana','watermelon','orange','papaya','pineapple']
+// let b="This is an empty string"
+
+// let find=a.map(
+//     function fruit(f){
+//         if (f===)
+//         {
+//             return a.splice(4,1)
+//         }
+//         else
+//         {
+//             return b
+//         }
+// }
+// )
+//  console.log(a,b);
+
+// let a=['apple','jackfruit','banana','watermelon','orange','papaya','pineapple']
+
+// function change(array,element){
+//     if(element == "" || array == [])
+//     {
+//         console.log("no elemtent or array found");
+//         return false
+//     }
+   
+//     let index = array.findIndex(
+
+//         function check(e)
+//         {
+//           return  e == element
+//         }
+//     )
+//     console.log(index);
+//     array.splice(index,1)
+//     console.log(array);
+//     return true
+// }
+// change(a,"orange")
+
+// let a=['apple','jackfruit','banana','watermelon','orange','papaya','pineapple']
+
+
+// while(i<=10){
+//     let num=i*5
+//     i++
+//     console.log(num);
+// }
+
+
+// do{
+//     ++i
+//     console.log(i);
+// }while(i<10)
+
+// for(let i=1;i<a.length;++i){
+//     console.log("the fruit is"+a[i]);
+// }
+
+// for ( let x of a){
+//     console.log("The fruit is"+x);
+// }
+
+// let a=[1,2,3,4,5,6,7,8,9,10]
+// for(let i=1;i<=10;i++){
+//     let num=i*3
+//     console.log(num);
+// }
+
+// let a=[1,2,3,4,5,6,7,8,9,10]
+
+// for( let x of a){
+//     let num=x*5
+//     console.log(num);
+// }
+
+// let bike=['Yamaha','RX','Honda','FZ','Duke','KTM','Splendour']
+
+// function make(arr,thing){
+//     if(thing == '' || arr ==[])
+//     {
+//         console.log('No thing is found');
+//         return false
+//     }
+//     let ind=arr.findIndex(
+//         function che(t){
+//             return t==thing
+//         }
+//     )
+//     console.log(ind);
+//     arr.splice(ind,1)
+//     console.log(arr);
+//     return true
+// }
+// make(bike,'Honda')
+
+const products = [
+    { name: "apple", quantity: 2, price: 150, order: 2, category: "fruit" },
+    { name: "banana", quantity: 5, price: 50, order: 3, category: "fruit" },
+    { name: "orange", quantity: 8, price: 80, order: 1, category: "fruit" },
+    { name: "grape", quantity: 3, price: 120, order: 4, category: "fruit" },
+    { name: "mango", quantity: 6, price: 100, order: 2, category: "fruit" },
+    { name: "carrot", quantity: 10, price: 60, order: 3, category: "vegetable" },
+    { name: "potato", quantity: 15, price: 40, order: 1, category: "vegetable" },
+    { name: "tomato", quantity: 7, price: 70, order: 2, category: "vegetable" },
+    { name: "onion", quantity: 12, price: 30, order: 4, category: "vegetable" },
+    { name: "broccoli", quantity: 4, price: 90, order: 3, category: "vegetable" },
+  ];
+
+// let x=5 ,y=10
+
+//   function add(a,b){
+//     return c=a+b
+//   }
+// add(x,y)
+// console.log(c);
+
+let age=8
+
+function baby(input){
+  if(input>20){
+    return "He is an adult"
+  }
+  else if(input>13){
+    return "He is teen"
+  }
+  else{
+  return "He is child"
+}
+}
+let x = baby(age)
+console.log(x);
